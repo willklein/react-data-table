40449da94d0c79dbccbcaf16e36e595b9d9f6389
Will Klein <will.klein@gmail.com>
2015-05-05 21:09:27 -0600

    Add propType validation to DataTableColumn

--- scripts/DataTableColumn.jsx
+++ scripts/DataTableColumn.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 
+const propTypes = {
+  align: React.PropTypes.string,
+  dataKey: React.PropTypes.string.isRequired,
+  label: React.PropTypes.string
+};
+
 export default class DataTableColumn extends React.Component {
   render() {
     return null;
   }
 }
+
+DataTableColumn.propTypes = propTypes;
 
 DataTableColumn.__DataTableColumn__ = true;
